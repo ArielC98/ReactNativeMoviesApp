6d9b0b76a4978785858357137fea872d32537ba6
ArielC98 <arielcalderon100@gmail.com>
2025-06-03 23:38:05 -0500

    API keys added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import './gesture-handler.native';
+import { NavigationContainer } from "@react-navigation/native"
 import { View, Text } from "react-native"
 
 
 export const App = () => {
   return (
-    <View>
-      <Text>Movies App</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>Movies App</Text>
+      </View>
+    </NavigationContainer>
   )
 }
