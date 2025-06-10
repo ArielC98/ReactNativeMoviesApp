3d50210ef38ed5d48d4d6be6be3305bc15b0a270
ArielC98 <arielcalderon100@gmail.com>
2025-06-09 23:20:06 -0500

    useMovie hook added

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,10 +1,21 @@
+
 import { Text, View } from "react-native"
+import { RootStackParams } from "../../navigation/Navigation";
+import { StackScreenProps } from "@react-navigation/stack";
 
+interface Props extends StackScreenProps <RootStackParams, 'Details'> {
 
-export const DetailsScreen = () => {
+}
+
+export const DetailsScreen = ({route}:Props) => {
+  
+  const {movieId} = route.params;
+
+  
+
   return (
     <View>
-      <Text>DetailsScreen</Text>
+      <Text>{movieId}</Text>
     </View>
   )
 }
